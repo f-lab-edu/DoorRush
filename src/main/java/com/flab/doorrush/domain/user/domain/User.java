d1253a6039469fd2e84655a58a3496bf260cf731
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2021-12-10 20:35:53 +0900

    [#39] 리뷰 반영

--- src/main/java/com/flab/doorrush/domain/user/domain/User.java
+++ src/main/java/com/flab/doorrush/domain/user/domain/User.java
@@ -2,13 +2,9 @@ package com.flab.doorrush.domain.user.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
-/**
- * @Data : @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode 의 기능 제공하는 어노테이션
- */
-
-@Data
+@Getter
 @Builder
 @AllArgsConstructor
 public class User {
