ce9e11840b6ab0f46e5bf5fd4d4bb2e3b2fb026e
promise_yoo <56250078+ypr821@users.noreply.github.com>
2021-12-15 04:27:27 +0900

    #5 리뷰 3차 반영 - 로그인 예외 추가

---
+++ src/main/java/com/flab/doorrush/domain/user/exception/loginException/IdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.flab.doorrush.domain.user.exception.loginException;
+
+
+public class IdNotFoundException extends RuntimeException {
+
+  public IdNotFoundException(String message) {
+    super(message);
+  }
+
+
+}
---
+++ src/main/java/com/flab/doorrush/domain/user/exception/loginException/SessionLoginIdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.flab.doorrush.domain.user.exception.loginException;
+
+
+public class SessionLoginIdNotFoundException extends RuntimeException {
+
+  public SessionLoginIdNotFoundException(String message) {
+    super(message);
+  }
+
+
+}
