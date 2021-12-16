86de3aaf6f1d6c6a33beb7794ee5ac9fd138d159
promise_yoo <56250078+ypr821@users.noreply.github.com>
2021-12-16 23:49:44 +0900

    #5 리뷰 4차 반영 - IdNotFoundException 주석 정리

--- src/main/java/com/flab/doorrush/domain/user/exception/InvalidPasswordException.java
+++ src/main/java/com/flab/doorrush/domain/user/exception/InvalidPasswordException.java
@@ -1,6 +1,5 @@
 package com.flab.doorrush.domain.user.exception;
 
-/*Base class for authentication exceptions which are caused by a particular user account status (locked, disabled etc).*/
 public class InvalidPasswordException extends RuntimeException {
 
   public InvalidPasswordException(String message) {
