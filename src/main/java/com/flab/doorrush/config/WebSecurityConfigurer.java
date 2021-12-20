ac287a5ba46b106dbe7082030086119ac2da76d1
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2021-12-20 23:05:38 +0900

    [#21] config 파일 디렉토리 변경

--- src/main/java/com/flab/doorrush/config/WebSecurityConfigurer.java
+++ src/main/java/com/flab/doorrush/config/WebSecurityConfigurer.java
@@ -1,4 +1,4 @@
-package com.flab.doorrush.domain.config;
+package com.flab.doorrush.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
