281d97d5dd0900902898d50583b2828ff0a2794f
promise_yoo <56250078+ypr821@users.noreply.github.com>
2022-01-15 12:37:25 +0900

    #15 자동로그인 기능 구현 - 대칭키 접근제어자 private 수정

--- src/main/java/com/flab/doorrush/global/util/SecurityUtils.java
+++ src/main/java/com/flab/doorrush/global/util/SecurityUtils.java
@@ -11,15 +11,24 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityUtils {
 
   final static String ALG = "AES/CBC/PKCS5Padding";
-  final static String AESKEY = "flabdoorrushboombayahboombayahbo";// 32byte
-  final static String AESIV = "jdpureummyeonjae"; //16byte
+  final private String AESKEY = "flabdoorrushboombayahboombayahbo";// 32byte
+  final private String AESIV = "jdpureummyeonjae"; //16byte
+
+  private String getAESKEY() {
+    return AESKEY;
+  }
+
+  private String getAESIV() {
+    return AESIV;
+  }
 
   public static String getEncryptedValue(String value) {
     String encryptedValueToString = "";
+    SecurityUtils securityUtils = new SecurityUtils();
     try {
       Cipher cipher = Cipher.getInstance(ALG);
-      SecretKeySpec keySpec = new SecretKeySpec(AESKEY.getBytes(), "AES");
-      IvParameterSpec ivParameterSpec = new IvParameterSpec(AESIV.getBytes());
+      SecretKeySpec keySpec = new SecretKeySpec(securityUtils.getAESKEY().getBytes(), "AES");
+      IvParameterSpec ivParameterSpec = new IvParameterSpec(securityUtils.getAESIV().getBytes());
       cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
       // 암호화 실행 및 인코딩 설정값 입력
       byte[] encryptedValue = cipher.doFinal(value.getBytes(StandardCharsets.UTF_8));
@@ -33,10 +42,11 @@ public class SecurityUtils {
 
   public static String getDecryptedValue(String encryptedValue) {
     String decryptedValue = "";
+    SecurityUtils securityUtils = new SecurityUtils();
     try {
       Cipher cipher = Cipher.getInstance(ALG);
-      SecretKeySpec secretKeySpec = new SecretKeySpec(AESKEY.getBytes(), "AES");
-      IvParameterSpec ivParameterSpec = new IvParameterSpec(AESIV.getBytes());
+      SecretKeySpec secretKeySpec = new SecretKeySpec(securityUtils.getAESKEY().getBytes(), "AES");
+      IvParameterSpec ivParameterSpec = new IvParameterSpec(securityUtils.getAESIV().getBytes());
       // 복호화 실행 및 인코딩 설정값 입력
       cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
       byte[] decodedBytes = Base64.getDecoder().decode(encryptedValue);
