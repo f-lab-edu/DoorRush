30e2856f520fbc5d09148dc03e156f1d99652daf
promise_yoo <56250078+ypr821@users.noreply.github.com>
2022-02-10 17:42:24 +0900

    #66 예외 중복 코드  정리

--- src/main/java/com/flab/doorrush/domain/authentication/aspect/CheckLoginAspect.java
+++ src/main/java/com/flab/doorrush/domain/authentication/aspect/CheckLoginAspect.java
@@ -17,7 +17,7 @@ public class CheckLoginAspect {
   private final HttpSession httpSession;
 
   @Before("@annotation(com.flab.doorrush.domain.authentication.annotation.CheckLogin)")
-  public void checkLogin() throws AuthenticationCredentialsNotFoundException {
+  public void checkLogin(){
     String currentId = (String) httpSession.getAttribute(AuthenticationService.LOGIN_SESSION);
     if (!StringUtils.hasLength(currentId)) {
       throw new AuthenticationCredentialsNotFoundException("로그인이 필요합니다.");
